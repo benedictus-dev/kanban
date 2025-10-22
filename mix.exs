6220b5d7f978f3e8686eade88c0505aeb06cbe85
Benedictus Yevu <benedictusyevu@Benedictuss-MacBook-Pro.local>
2025-10-22 00:50:58 +0000

    commit

--- mix.exs
+++ mix.exs
@@ -88,7 +88,12 @@ defmodule Kanban.MixProject do
         "esbuild kanban --minify",
         "phx.digest"
       ],
-      precommit: ["compile --warning-as-errors", "deps.unlock --unused", "format  --check-formatted", "test --trace"]
+      precommit: [
+        "compile --warning-as-errors",
+        "deps.unlock --unused",
+        "format  --check-formatted",
+        "test --trace"
+      ]
     ]
   end
 end
